be0191393567583497a1a5a2422615a8cdb7f51a
harapan1231 <senang.harapan@gmail.com>
2018-01-03 23:22:36 +0900

    Add script files

---
+++ python/input-calc-print.py
@@ -0,0 +1,7 @@
+first_number = input("Input the first number: ")
+second_number = input("Input the second number: ")
+
+sum = int(first_number) + int(second_number)
+
+print("To sum up to: " + str(sum))
+
---
+++ python/input-print.py
@@ -0,0 +1,20 @@
+x = input("Enter your name: ")
+print(x + " is one of the best people on earth.")
+
+x = input("Enter one of your good friend's name: ")
+print(x + " thinks you're a good friend.")
+
+x = input ( "Enter your favorite family member: ")
+print(x + " loves you so much.")
+
+x = input("Enter one of your favorite food: ")
+print(x + " is made out of good things.")
+
+x = input ( "Enter your favorite animal: ")
+print(x + " looks like a chunk of mud shaped into an animal structure.")
+
+x = input ( "Enter your favorite color: ")
+print(x + " is one of the best color in the whole universe.")
+
+print("Thank you for using!")
+ 
